94345bd9ae6adb6f1cc1bdf03314af725c6589f7
Eric W. Shannon <eric.shannon@ku.edu>
2019-02-23 14:05:37 -0500

    Update from last nights game.

--- R/ncaa_bracket.R
+++ R/ncaa_bracket.R
@@ -8,7 +8,7 @@ require(psych)
 
 `%!in%` <- negate(`%in%`)
 
-data2 <- openxlsx::read.xlsx("../data/20190222.xlsx", colNames = TRUE, startRow = 2)
+data2 <- openxlsx::read.xlsx("../data/20190223.xlsx", colNames = TRUE, startRow = 2)
 data2 <- data2[, -1]
 conf <- read.csv("../data/teams.csv", header = FALSE)
 rownames(data2) <- data2[, 1]
