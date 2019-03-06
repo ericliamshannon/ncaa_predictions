23ce0fe7bd5b5cd2ce49a47ca88469393cabe259
Eric W. Shannon <eric.shannon@ku.edu>
2019-03-06 06:59:28 -0500

    Updating with yesterdays scores

--- R/ncaa_bracket.R
+++ R/ncaa_bracket.R
@@ -1,5 +1,5 @@
 ## Created by: Eric William Shannon, PhD
-## Date modified: 20190303
+## Date modified: 20190304
 
 require(tidyverse)
 require(ggplot2)
@@ -8,7 +8,7 @@ require(psych)
 
 `%!in%` <- negate(`%in%`)
 
-data2 <- openxlsx::read.xlsx("../data/20190304.xlsx", colNames = TRUE, startRow = 2)
+data2 <- openxlsx::read.xlsx("../data/20190305.xlsx", colNames = TRUE, startRow = 2)
 data2 <- data2[, -1]
 conf <- read.csv("../data/teams.csv", header = FALSE)
 rownames(data2) <- data2[, 1]
@@ -89,5 +89,5 @@ ggplot(ncaa_predicts, aes(team, simulated)) +
   geom_bar(stat = "identity", aes(fill = team)) +
   scale_color_fivethirtyeight() + theme_fivethirtyeight() +
   theme(legend.position = "none", 
-        axis.text.x = element_text(angle = 45, hjust = 1, size = 5))
-  
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 5)) +
+  geom_hline(yintercept = 1.298176, linetype = "dashed", alpha = 0.5)
