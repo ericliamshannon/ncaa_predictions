f27941242a49282e416fdf1c339333bbf560faac
Eric W. Shannon <eric.shannon@ku.edu>
2019-03-07 11:30:31 -0500

    Updating with last nights scores.

--- R/ncaa_bracket.R
+++ R/ncaa_bracket.R
@@ -1,5 +1,5 @@
 ## Created by: Eric William Shannon, PhD
-## Date modified: 20190304
+## Date modified: 20190307
 
 require(tidyverse)
 require(ggplot2)
@@ -8,7 +8,7 @@ require(psych)
 
 `%!in%` <- negate(`%in%`)
 
-data2 <- openxlsx::read.xlsx("../data/20190305.xlsx", colNames = TRUE, startRow = 2)
+data2 <- openxlsx::read.xlsx("../data/20190307.xlsx", colNames = TRUE, startRow = 2)
 data2 <- data2[, -1]
 conf <- read.csv("../data/teams.csv", header = FALSE)
 rownames(data2) <- data2[, 1]
